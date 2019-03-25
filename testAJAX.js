a1bd164e5e026279711da8470460547fd37da1aa
georgepyne <georgepyne@hotmail.co.uk>
2019-03-25 12:28:52 -0400

    AJAX test

---
+++ testAJAX.js
@@ -0,0 +1,66 @@
+var xhr;
+
+function callDivChange(){
+    xhr = new XMLHttpRequest();
+    var filename = document.getElementById("filename").value;
+    xhr.open("GET", filename, true);
+    xhr.onreadystatechange =  processDivChange;
+    xhr.send();
+}
+
+function processDivChange(){
+    if (xhr.readyState < 4){
+        document.getElementById('div1').innerHTML = "Loading...";
+        // alert(xhr.status) // Use
+    } else if (xhr.status === 200 && xhr.status < 300){
+        // http status between 200-299 are all successful
+        document.getElementById('div1').innerHTML = xhr.responseText;
+    }
+}
+
+var xhrNode;
+
+// function callDivNodeJSChange() {
+//     xhrNode = new XMLHttpRequest();
+//     var url = "http://developer.cege.ucl.ac.uk:" + httpPortNumber;
+//     xhrNode.open("GET", url, true);
+//     xhrNode.onreadystatechange = processDivNodeJSChange;
+//     try {
+//         xhrNode.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+//     } catch (err) {
+//         //this only works on IE browser
+//     }
+//     xhrNode.send();
+// }
+//
+// function processDivNodeJSChange() {
+//     if (xhrNode.readyState < 4){
+//         document.getElementById('ajaxtest').innerHTML = "Loading..."
+//     } else if (xhrNode.readyState === 4){
+//         if (xhrNode.status == 200 && xhr.status < 300){
+//             document.getElementById('ajaxtest').innerHTML = xhrNode.responseText;
+//         }
+//     }
+// }
+
+function callDivNodeJSChange() {
+    xhrNode = new XMLHttpRequest();
+    // var filename = document.getElementById("filename").value;
+    var url = "http://developer.cege.ucl.ac.uk:" + httpPortNumber;
+    xhrNode.open("GET", url, true);
+    xhrNode.onreadystatechange = processDivNodeJSChange;
+    xhrNode.send();
+}
+
+function processDivNodeJSChange() {
+    if (xhrNode.readyState < 4) {
+        document.getElementById('ajaxtest').innerHTML = "Loading...";
+    } else if (xhrNode.readyState === 4) {
+        if (xhrNode.status == 200 && xhrNode.status < 300) {
+            document.getElementById('ajaxtest').innerHTML = xhrNode.responseText;
+        }
+    }
+}
+
+
+
