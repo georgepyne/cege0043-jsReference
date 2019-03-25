5704f5e6b2c9abe29bb5cb114cc11d601abeeb33
georgepyne <georgepyne@hotmail.co.uk>
2019-03-25 12:28:36 -0400

    AJAX test

--- utilities.js
+++ utilities.js
@@ -2,15 +2,23 @@
 var httpPortNumber;
 var httpsPortNumber;
 
+
 function getPort(){
     var xhr = new XMLHttpRequest();
-    xhr.addEventListener("load", () => {
+    xhr.addEventListener("load", function ()  {
         var parser = new DOMParser();
         var doc = parser.parseFromString(xhr.responseText, "application/xml");
         // qualified name "node-port-http(s)" is defined recursively and qualifies it
-        httpPortNumber = doc.getElementsByTagName("node-port-http").item(0).textContent;
-        // access first item by index 0
-        httpsPortNumber = doc.getElementsByTagName("node-port-https").item(0).textContent;
+        // try {
+        //     httpPortNumber = doc.getElementsByTagName("node-port-http").item(0).textContent;
+        //     // access first item by index 0
+        //     alert("http port number = " + httpPortNumber)
+        // } catch (err) {
+        //     httpsPortNumber = doc.getElementsByTagName("node-port-https").item(0).textContent;
+        //     alert("https port number = " + httpPortNumber)
+            httpPortNumber = doc.getElementsByTagName("node-port-http").item(0).textContent;
+            httpsPortNumber = doc.getElementsByTagName("node-port-https").item(0).textContent;
+        // }
         });
 
     // Config location is dependent whether user is on browser or mobile
@@ -18,5 +26,6 @@ function getPort(){
     var configLocation = "res/port.xml";
     xhr.open("get", configLocation, true); // method, location and async bool
     xhr.send();
+    // alert("Getting Port!")
 }
 
